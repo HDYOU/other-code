69d90f15a5ac070eb1f8d23fb7e5cf0740512720
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-05-09 12:23:37 +0800

    更新 validate_chapter_content_html.js

--- legado/validate_chapter_content_html.js
+++ legado/validate_chapter_content_html.js
@@ -2,6 +2,7 @@
  * 检验并获取章节内容,
  * @returns {*|string}
  */
+chapter_word_count=0;
 function validate_chapter_content() {
     let txt = String(result)
     txt = txt.replace(/亲,点击进去,给个好[^\n<>]*漂亮的老婆哦!|手机站全新改版[^\n<>]*广告清新阅读！|最新网址[^\n<>]*|[(（]?本章完[)）]?|推荐.*新书[^\n<>]*|手机用户[^\n<>]*阅读体验。/g,"")
@@ -10,10 +11,11 @@ function validate_chapter_content() {
     }
     // 中文字符数统计
     let matches = txt.match(/[\u4e00-\u9fa5\u2000-\u3020\uff00-\uff60?!,]/g);
-    let len = matches ? matches.length : 0;
+    chapter_word_count = matches ? matches.length : 0;
     //java.log(JSON.stringify(matches))
+    //java.log(`${chapter.title} 字数: ${chapter_word_count}`)
     // 起点小说内容防盗
-    if (1000 < len && len < 1400) {
+    if (1000 < chapter_word_count && chapter_word_count < 1400) {
         return ""
     }
     return txt;
