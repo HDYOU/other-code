2cb51c70f5d19029cd48c8c5e967fe54f57bf622
HDYOU <32186506+HDYOU@users.noreply.github.com>
2026-05-07 17:47:55 +0800

    fix

---
+++ legado/update_chapter_info.js
@@ -0,0 +1,34 @@
+function update_chapter_info(base_chapter_list, other_chapter_list){
+    
+    if(!other_chapter_list || other_chapter_list.length < 1) return;
+    chapter_title_info_dict={};
+    reg_1=/第\s*[\d〇零一二两三四五六七八九十百千万壹贰叁肆伍陆柒捌玖拾佰仟]+\s*章\s*/
+    reg_2=/[\(（].*/g
+    for (var i = 0; i <other_chapter_list.length; i++) {
+      var item = other_chapter_list[i];
+      var s_txt=String(item.text);
+      chapter_title_info_dict[item.text]=item.info;
+      part_1=s_txt.replace(reg_1, "");
+      chapter_title_info_dict[part_1]=item.info;
+      part_2=part_1.replace(reg_2, "")
+      chapter_title_info_dict[part_2]=item.info;
+    }
+    
+    for (var i = 0; i <base_chapter_list.length; i++) {
+      var item = base_chapter_list[i];
+      txt_list=[];
+      var s_txt=String(item.text);
+      txt_list.push(s_txt);
+      part_1=s_txt.replace(reg_1, "");
+      txt_list.push(part_1);
+      txt_list.push(part_1.replace(reg_2, ""));
+      
+      for (var j = 0; j <txt_list.length; j++) {
+          tmp_key=txt_list[j];
+          item.info = chapter_title_info_dict[tmp_key] || "";
+          if( item.info != "") break;
+       }
+      
+    }
+  
+}
