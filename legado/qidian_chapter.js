015c885d37b411a95ca14d4de590eaad42c5cefa
HDYOU <32186506+HDYOU@users.noreply.github.com>
2026-05-07 16:49:08 +0800

    qidian_chapter.js

--- legado/qidian_chapter.js
+++ legado/qidian_chapter.js
@@ -10,16 +10,27 @@ function get_qd_chapter_list() {
 function get_qd_chapter_list_by_bookid(qd_bookid) {
     cache_qd_chapter_key = `qidian_chapter_${qd_bookid}`;
     cache_qd_chapter = cache.get(cache_qd_chapter_key);
+    java.log("cache_qd_chapter:"+cache_qd_chapter)
     if (cache_qd_chapter && cache_qd_chapter != "") {
         try {
-            return JSON.parse(cache_qd_chapter);
+            qd_chapter = JSON.parse(cache_qd_chapter);
+            if(qd_chapter && qd_chapter.length > 0) return qd_chapter
         } catch (e) {
             java.log(e)
         }
     }
 
-    tocUrl = `https://m.qidian.com/book/${qd_bookid}/catalog/`;
+    option={
+      "headers":{
+        "Cookie": java.getCookie("https://m.qidian.com/"),
+        "Referer": "https://m.qidian.com/",
+        "User-Agent":"Mozilla/5.0 (Linux; U; Android 13; zh-Hans-CN; PFJM10 Build/TP1A.220905.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/135.0.4896.58 Quark/6.13.6.581 Mobile Safari/537.36",
+        "Accept-Language":"zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+      }
+    }
+    tocUrl = `https://m.qidian.com/book/${qd_bookid}/catalog/,${JSON.stringify(option)}`;
     html = java.ajax(tocUrl);
+    //java.log("qd html:\n"+html)
     java.setContent(html, tocUrl);
     eles = java.getElements(".y-list__item@a,._chapterBar_fps9g_592");
     //java.log("eles:"+JSON.stringify(eles));
@@ -53,6 +64,6 @@ function get_qd_chapter_list_by_bookid(qd_bookid) {
     }
 
     cache_hour = 12; // 缓存12小时
-    cache.put(cache_qd_chapter_key, JSON.stringify(chapter_list, cache_hour * 60*60));
+    if(chapter_list.length > 0) cache.put(cache_qd_chapter_key, JSON.stringify(chapter_list, cache_hour * 60*60));
     return chapter_list;
 }
